18b64ff6ada3815b13d5f7b69d84e00a1bb7cf3f
Manojkirugavaluthibbegowda <manojktireland@gmail.com>
2024-12-14 19:01:28 +0000

    Made all endpoints secured by authentication by default, Exclusions on specific endpoints, Tested working of authentication on the endpoints

--- src/main/java/com/myproject/ecommerce/backend/api/security/WebSecurityConfig.java
+++ src/main/java/com/myproject/ecommerce/backend/api/security/WebSecurityConfig.java
@@ -5,14 +5,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFilter;
 
 @Configuration
 public class WebSecurityConfig {
 
+    private JWTRequestFilter jwtRequestFilter;
+
+    public WebSecurityConfig(JWTRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
+
     @Bean
     public SecurityFilterChain filterchain(HttpSecurity http) throws Exception {
         http.csrf(csrf->csrf.disable()).cors(cors->cors.disable());
-        http.authorizeHttpRequests(auth->auth.anyRequest().permitAll());
+        http.addFilterBefore(jwtRequestFilter, AuthenticationFilter.class);
+        http.authorizeHttpRequests(auth->auth
+                .requestMatchers("/product", "/auth/register", "/auth/login").permitAll()
+                .anyRequest().authenticated());
         return http.build();
     }
 }
